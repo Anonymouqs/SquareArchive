06ac55c5d6aea7d146efe46e05a174486360270f
Anonymouqs <Jehber.ibarra@gmail.com>
2018-08-25 00:59:38 +0900

    Smooth movement

--- Root/Entity/PC/PC.gd
+++ Root/Entity/PC/PC.gd
@@ -1,24 +1,35 @@
 extends KinematicBody2D
 
 export var Up = Vector2(0,-1)
-export var Gravity = 10
-export var Jump = 10
-export var Speed = 10
+export var Gravity = 50
+export var Jump = 1000
+export var JumpMax = 2
+export var Speed = 500
+export var Accel = 20
 
-var motion = vector2()
+var motion = Vector2()
+var jumpcount = 1
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
 	
-	motion.y -= Gravity
+	motion.y += Gravity
 	
-	if Input.is_action_pressed("ui_up"):
-		motion.y =Jump
+	if Input.is_action_just_pressed("ui_up") and jumpcount < JumpMax:
+		#print(jumpcount)
+		jumpcount+=1
+		motion.y = -Jump
+	#print(jumpcount)
+	
 	if Input.is_action_pressed("ui_left"):
-		motion.x -= Speed
-	if Input.is_action_pressed("ui_right"):
-		motion.x += Speed
-		
-	move_and_slide(motion,Up)
+		motion.x = min(motion.x-Accel, -Speed)
+	elif Input.is_action_pressed("ui_right"):
+		motion.x = max(motion.x+Accel, Speed)
+	else:
+		motion.x = lerp(motion.x, 0, 0.15)
+	if is_on_floor():
+		jumpcount = 1
+	
+	motion = move_and_slide(motion,Up)
